60d27138955318a29a607be4c8bd5a499a5de4b9
KivilevaXenia <kivilevaxenia@gmail.com>
2020-01-14 18:40:06 +0300

    package.json

---
+++ stub/pages/style/style.bemdecl.js
@@ -0,0 +1,13 @@
+exports.blocks = [
+    { name: 'page' },
+    { name: 'theme' },
+    { name: 'header' },
+    { name: 'layout' },
+    { name: 'footer' },
+    { name: 'payment' },
+    { name: 'warning' },
+    { name: 'product' },
+    { name: 'history' },
+    { name: 'event' },
+    { name: 'cover' }
+];
